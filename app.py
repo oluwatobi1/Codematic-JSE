1a8e43e14ac29af3684416af5627b1a60305f69d
tobi_akin <akintunlesetobi@gmail.com>
2021-07-16 17:45:12 +0100

    app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ def great_circle_distance():
     url_location_params = helper_scripts.get_location_params()
     if url_location_params['is_valid']==True:
         latitude1, latitude2, longitude1, longitude2  = url_location_params["data"]
-
         distance = helper_scripts.calculate_distance(latitude1, latitude2, longitude1, longitude2)
         
         return jsonify(distance), 200
