75a35fc2d876d763274a94dfd18a4ca631940106
tobi_akin <akintunlesetobi@gmail.com>
2021-07-16 19:41:20 +0100

    added comments

--- app.py
+++ app.py
@@ -5,26 +5,26 @@ app = Flask(__name__)
 
 @app.route('/great_circle_distance/')
 def great_circle_distance():
-    # validate url params from helper_scripts
     if request.method == "GET":
+         # validate url params from helper_scripts
         url_location_params = helper_scripts.get_location_params()
         if url_location_params['is_valid']==True:
             latitude1, latitude2, longitude1, longitude2  = url_location_params["data"]
+            # calculate distance from helper
             distance = helper_scripts.calculate_distance(latitude1, latitude2, longitude1, longitude2)
             
             return jsonify(distance), 200
         else:
+            # return error from helper
             error_message = url_location_params["data"]
             return {'error': error_message}, 400
 
     
-
-
-
-    
-
-       
-    
+  
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
+    
+
+       
+ 
