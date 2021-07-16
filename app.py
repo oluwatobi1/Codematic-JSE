c242c2e38f75ed7a70163767b437e74ac7f7baf6
tobi_akin <akintunlesetobi@gmail.com>
2021-07-16 22:31:44 +0100

    updated docstrings

--- app.py
+++ app.py
@@ -5,18 +5,25 @@ app = Flask(__name__)
 
 @app.route('/great_circle_distance/')
 def great_circle_distance():
+    """
+    Implements GET Request Method
+
+    Parameters:
+    
+    Returns:
+    data (json): returns serialized response of calculated distance  
+    """
     if request.method == "GET":
          # validate url params from helper_scripts
         url_location_params = helper_scripts.get_location_params()
         if url_location_params['is_valid']==True:
             latitude1, latitude2, longitude1, longitude2  = url_location_params["data"]
+
             # calculate distance from helper
             distance = helper_scripts.calculate_distance(latitude1, latitude2, longitude1, longitude2)
 
             # round to 2dp
             response = str(round(distance,2))+ " KM"
-
-
             return jsonify(response), 200
         else:
             # return error from helper
@@ -25,9 +32,9 @@ def great_circle_distance():
 
     
   
-
-if __name__ == "__main__":
-    app.run(debug=True)
+# uncomment this to run locally
+# if __name__ == "__main__":
+#     app.run(debug=True)
     
 
        
